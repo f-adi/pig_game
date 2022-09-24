503540fb33a414b3dbb72580c4f1ab7d66894a97
f-adi <fadi.baaklini@gmail.com>
2022-09-24 17:56:50 -0400

    player change

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict';
 
 //Selecting element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -24,4 +32,18 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   //check for rolled  one. If true switch to next player
+
+  if (dice !== 1) {
+    //Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
